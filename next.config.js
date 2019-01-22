cd6bd7857a5f10111da9c4cf19734a3440eb7029
Walter Zalazar <walter.zalazar.mdp@gmail.com>
2019-01-22 18:15:11 -0300

    fix

--- next.config.js
+++ next.config.js
@@ -10,42 +10,17 @@ module.exports = (phase) => {
     // Config used to run in production.
     return {}
   }
-
-  const withPlugins = require('next-compose-plugins')
-  const withImages = require('next-images')
-  const withFonts = require('next-fonts')
-  const withSass = require('@zeit/next-sass')
-  const withCSS = require('@zeit/next-css')
-
-  return withPlugins([
-    [
-      withSass,
-      {
-        cssModules: true,
-        cssLoaderOptions: {
-          importLoaders: 1,
-          localIdentName: '[local]'
-        }
-      }
-    ],
-
-    [
-      withCSS,
-      {
-        cssModules: true,
-        cssLoaderOptions: {
-          importLoaders: 1,
-          localIdentName: '[local]'
-        }
-      }
-    ],
-
-    [
-      withImages,
-      {
-        inlineImageLimit: 0
-      }
-    ],
-    withFonts
-  ])
 }
+
+const withPlugins = require('next-compose-plugins')
+const withImages = require('next-images')
+const withFonts = require('next-fonts')
+const withSass = require('@zeit/next-sass')
+const withCSS = require('@zeit/next-css')
+
+module.exports = withPlugins([
+  withFonts,
+  withSass,
+  withCSS,
+  withImages
+])
