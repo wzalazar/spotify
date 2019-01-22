7a73092271d25c1eaaa94306a6692faa4d8d8a03
Walter Zalazar <walter.zalazar.mdp@gmail.com>
2019-01-22 19:17:21 -0300

    test

--- next.config.js
+++ next.config.js
@@ -1,12 +1,28 @@
+const { PHASE_PRODUCTION_SERVER } =
+  process.env.NODE_ENV === 'development'
+    ? {}
+    : !process.env.NOW_REGION
+    ? require('next/constants')
+    : require('next-server/constants')
+
 const withPlugins = require('next-compose-plugins')
 const withImages = require('next-images')
 const withFonts = require('next-fonts')
 const withSass = require('@zeit/next-sass')
 const withCSS = require('@zeit/next-css')
 
-module.exports = withPlugins([
-  withFonts,
-  withSass,
-  withCSS,
-  withImages
-])
+const nextConfig = {}
+
+module.exports = (phase, {defaultConfig}) => {
+  if (phase === PHASE_PRODUCTION_SERVER) {
+    // Config used to run in production.
+    return {}
+  }
+
+  return withPlugins([
+    withFonts,
+    withSass,
+    withCSS,
+    withImages
+  ], nextConfig)(phase, { defaultConfig })
+}
