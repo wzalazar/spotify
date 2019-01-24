f5246c64c755da51e6b1383c6e1853687c09ce11
Walter Zalazar <walter.zalazar.mdp@gmail.com>
2019-01-24 15:24:50 -0300

    chore: Refactor <Input />  (#41)

--- components/Input/Input.js
+++ components/Input/Input.js
@@ -1,47 +1,18 @@
-import { Component } from 'react'
-import PropTypes from 'prop-types'
+
 import { noop } from 'lodash'
 
-export default class Input extends Component {
-  static propTypes = {
-    name: PropTypes.string.isRequired,
-    placeholder: PropTypes.string,
-    value: PropTypes.string,
-    type: PropTypes.string,
-    className: PropTypes.string,
-    onChange: PropTypes.func,
-  }
-
-  static defaultProps = {
-    name: '',
-    placeholder: '',
-    type: 'text',
-    className: '',
-    onChange: noop,
-  }
-
-  constructor() {
-    super()
-  }
-
-  onChange(event) {
-    const value = event.target.value
-    const { onChange } = this.props
-    onChange(value)
-  }
-
-  render() {
-    const { name, placeholder, value, type, className } = this.props
-
-    return (
-      <div className={'Input'}>
-        <input className={className}
-          onChange={this.onChange.bind(this)}
-          name={name}
-          value={value}
-          placeholder={placeholder}
-          type={type} />
-      </div>
-    )
-  }
+const trigger = (event, callback) => {
+  const value = event.target.value
+  callback(value)
 }
+
+export const Input = ({ name = '', placeholder = '', value = '', type = 'text', className = '', onChange = noop }) => (
+  <div className={'Input'}>
+    <input className={className}
+      onChange={(e) => trigger(e, onChange)}
+      name={name}
+      value={value}
+      placeholder={placeholder}
+      type={type} />
+  </div>
+)
