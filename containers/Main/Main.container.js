a70b4a72052f08a8a92b304adb9c433a3bd46d27
Walter Zalazar <walter.zalazar.mdp@gmail.com>
2019-02-12 20:30:31 -0300

    remove comments

--- containers/Main/Main.container.js
+++ containers/Main/Main.container.js
@@ -1,28 +1,6 @@
-// import { Component } from 'react'
-// import { connect } from 'react-redux'
 import { Main } from '../../components/Main/Main'
 import { SearchContainer } from '../Search/Search.container'
 import { ResultsContainer } from '../Results/Results.container'
-
-// @connect(({ results }) => ({
-//   results,
-// }))
-// class MainContainer extends Component {
-//   constructor() {
-//     super()
-//   }
-
-//   render() {
-//     return (
-//       <Main>
-//         <Search />
-//         <Results />
-//       </Main>
-//     )
-//   }
-// }
-
-// export default MainContainer
 
 export const MainContainer = () => (
   <Main>
