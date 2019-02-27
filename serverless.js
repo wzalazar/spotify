7574dc6b2cf58ca9d5b001ed08ea0e591247c031
Walter Zalazar <walter.zalazar.mdp@gmail.com>
2019-02-27 16:56:50 -0300

    wip

--- serverless.js
+++ serverless.js
@@ -1,4 +1,10 @@
 const serverless = require('serverless-http')
 const server = require('./server')
 
-module.exports = serverless(server)
+// module.exports = serverless(server)
+
+module.exports = (req, res) => {
+  const { query } = parse(req.url, true)
+  const { name = 'World' } = query
+  res.end(`Hello ${name}!`)
+}
