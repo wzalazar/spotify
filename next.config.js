9affdd28a4e9abc5f04377f22ff7316903173903
Walter Zalazar <walter.zalazar.mdp@gmail.com>
2019-02-27 17:43:41 -0300

    wip

--- next.config.js
+++ next.config.js
@@ -1,67 +1,35 @@
-const path = require('path')
+const withPlugins = require('next-compose-plugins')
+const withImages = require('next-images')
+const withFonts = require('next-fonts')
+const withSass = require('@zeit/next-sass')
+const withCSS = require('@zeit/next-css')
 
-/* eslint-disable no-nested-ternary */
-const { PHASE_PRODUCTION_SERVER } =
-  process.env.NODE_ENV === 'development'
-    ? {}
-    : !process.env.NOW_REGION
-      ? require('next/constants')
-      : require('next-server/constants')
-
-const nextConfig = {
-  webpack: config => {
-    config.resolve.alias = {
-      Components: path.resolve(__dirname, 'Components'),
-    }
-
-    return config
-  },
-}
-
-module.exports = (phase, { defaultConfig }) => {
-  if (phase === PHASE_PRODUCTION_SERVER) {
-    // Config used to run in production.
-    return {}
-  }
-
-  const withPlugins = require('next-compose-plugins')
-  const withImages = require('next-images')
-  const withFonts = require('next-fonts')
-  const withSass = require('@zeit/next-sass')
-  const withCSS = require('@zeit/next-css')
-
-  return withPlugins(
-    [
-      [
-        withSass,
-        {
-          cssModules: true,
-          cssLoaderOptions: {
-            importLoaders: 1,
-            localIdentName: '[local]',
-          },
-        },
-      ],
-
-      [
-        withCSS,
-        {
-          cssModules: true,
-          cssLoaderOptions: {
-            importLoaders: 1,
-            localIdentName: '[local]',
-          },
-        },
-      ],
-
-      [
-        withImages,
-        {
-          inlineImageLimit: 0,
-        },
-      ],
-      withFonts,
-    ],
-    nextConfig
-  )(phase, { defaultConfig })
-}
+module.exports = withPlugins([
+  [
+    withSass,
+    {
+      cssModules: true,
+      cssLoaderOptions: {
+        importLoaders: 1,
+        localIdentName: '[local]',
+      },
+    },
+  ],
+  [
+    withCSS,
+    {
+      cssModules: true,
+      cssLoaderOptions: {
+        importLoaders: 1,
+        localIdentName: '[local]',
+      },
+    },
+  ],
+  [
+    withImages,
+    {
+      inlineImageLimit: 0,
+    },
+  ],
+  withFonts,
+])
